69116e8fa47f098d2526b1a80452bcd8cf3ff847
Pedro-Afonso <pedro.766@hotmail.com>
2022-12-03 10:13:07 -0300

    feat: Add redux provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
 
 import { StepProvider } from "../context/StepContext";
 import { AppAuthProvider } from "../context";
@@ -6,16 +7,19 @@ import "../styles/globals.scss";
 import "../config/firebase";
 
 import { getAuth } from "firebase/auth";
+import { store } from "../config/store";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const auth = getAuth();
 
   return (
-    <AppAuthProvider auth={auth}>
-      <StepProvider>
-        <Component {...pageProps} />
-      </StepProvider>
-    </AppAuthProvider>
+    <Provider store={store}>
+      <AppAuthProvider auth={auth}>
+        <StepProvider>
+          <Component {...pageProps} />
+        </StepProvider>
+      </AppAuthProvider>
+    </Provider>
   );
 }
 
