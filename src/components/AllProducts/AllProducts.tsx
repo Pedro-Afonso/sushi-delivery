be6ae531f9db4322828bf08b132ee39187b5e365
Pedro-Afonso <pedro.766@hotmail.com>
2022-12-03 10:15:28 -0300

    feat: Add ProductCard component

--- src/components/AllProducts/AllProducts.tsx
+++ src/components/AllProducts/AllProducts.tsx
@@ -1,15 +1,20 @@
 import styles from "./AllProducts.module.scss";
 import db from "../../../db.json";
+import { CardMenu } from "../CardMenu";
+import { useEffect, useState } from "react";
+import { ProductMenu } from "./ProductMenu";
+import useCloseMenu from "../../hooks/useCloseMenu";
+import { useMenu } from "../../hooks/useMenu";
+import { ProductCard } from "./ProductCard";
 
 export const AllProducts = () => {
   const { products } = db;
+
   return (
     <>
       <section className={styles.section}>
         {products.map((product) => (
-          <div key={product._id} className={styles.card}>
-            <h2>{product.name}</h2>
-          </div>
+          <ProductCard key={product._id} product={product} />
         ))}
       </section>
     </>
---
+++ src/components/AllProducts/ProductCard/ProductCard.tsx
@@ -0,0 +1,30 @@
+import { useMenu } from "../../../hooks";
+import { ProductMenu } from "../ProductMenu";
+import styles from "./ProductCard.module.scss";
+
+interface IProductCardProps {
+  product: {
+    _id: number;
+    name: string;
+    price: number;
+    url: string;
+    description: string;
+    ingredients: string[];
+  };
+}
+
+export const ProductCard: React.FC<IProductCardProps> = ({ product }) => {
+  const { isVisible, toggleMenuHandler } = useMenu();
+
+  return (
+    <>
+      <div key={product._id} className={styles.card}>
+        <div className={styles.edit}>
+          <button onClick={toggleMenuHandler}>...</button>
+          {isVisible && <ProductMenu toggleMenuHandler={toggleMenuHandler} />}
+        </div>
+        <h2>{product.name}</h2>
+      </div>
+    </>
+  );
+};
