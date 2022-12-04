3945e3b7c280a66da57fed77901ea5fe348db868
Pedro-Afonso <pedro.766@hotmail.com>
2022-12-04 15:29:35 -0300

    feat: Add data parameter

--- src/components/AllProducts/ProductMenu/ProductMenu.tsx
+++ src/components/AllProducts/ProductMenu/ProductMenu.tsx
@@ -6,21 +6,24 @@ import {
   toggleUpdateProduct,
 } from "../../../slices/modalSlice";
 import { CardMenu } from "../../CardMenu";
+import { IProduct } from "../../interface";
 
 interface IProductMenuProps {
   toggleMenuHandler: () => void;
+  data: IProduct | null;
 }
 
 export const ProductMenu: React.FC<IProductMenuProps> = ({
   toggleMenuHandler,
+  data = null,
 }) => {
   const menuRef = useCloseMenu(toggleMenuHandler);
   const dispatch = useAppDispatch();
 
   return (
     <CardMenu ref={menuRef}>
-      <li onClick={() => dispatch(toggleUpdateProduct(null))}>Editar</li>
-      <li onClick={() => dispatch(toggleRemoveProduct(null))}>Excluir</li>
+      <li onClick={() => dispatch(toggleUpdateProduct(data))}>Editar</li>
+      <li onClick={() => dispatch(toggleRemoveProduct(data))}>Excluir</li>
     </CardMenu>
   );
 };
