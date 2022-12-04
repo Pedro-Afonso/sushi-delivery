caa2d74aa0d4b7c7e3550d29ff47f3ea19185dd5
Pedro-Afonso <pedro.766@hotmail.com>
2022-12-04 15:27:35 -0300

    feat: Add addDocument function

--- src/hooks/useFirestore.tsx
+++ src/hooks/useFirestore.tsx
@@ -1,9 +1,20 @@
-import { doc, updateDoc } from "firebase/firestore";
+import { addDoc, collection, doc, updateDoc } from "firebase/firestore";
 import { db } from "../config/firebase";
 
 export const useFirestore = (docCollection: any) => {
+  const addDocument = async (data: { [x: string]: any }) => {
+    const coll = collection(db, docCollection);
+
+    try {
+      await addDoc(coll, data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const updateDocument = async (id: string, data: { [x: string]: any }) => {
     const docRef = doc(db, docCollection, id);
+
     try {
       await updateDoc(docRef, data);
     } catch (error) {
@@ -11,5 +22,5 @@ export const useFirestore = (docCollection: any) => {
     }
   };
 
-  return { updateDocument };
+  return { addDocument, updateDocument };
 };
