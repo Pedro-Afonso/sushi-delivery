2350cb7f85bdb371bb864ef44a7d271c9ad85cf4
Pedro-Afonso <pedro.766@hotmail.com>
2022-12-05 09:04:50 -0300

    feat: Implement deleteDocument functionality

--- src/components/AllProducts/Modal/RemoveProduct/RemoveProduct.tsx
+++ src/components/AllProducts/Modal/RemoveProduct/RemoveProduct.tsx
@@ -1,6 +1,12 @@
-import { useAppDispatch, useAppSelector } from "../../../../hooks";
+import {
+  useAppDispatch,
+  useAppSelector,
+  useFirestore,
+} from "../../../../hooks";
 import { toggleRemoveProduct } from "../../../../slices/modalSlice";
 import { Modal } from "../../../Modal";
+
+import styles from "../../../Modal/Modal.module.scss";
 
 export const RemoveProduct = () => {
   const dispatch = useAppDispatch();
@@ -8,13 +14,29 @@ export const RemoveProduct = () => {
     (state) => state.modal.removeProduct
   );
 
+  const { deleteDocument } = useFirestore("products");
+
+  const handleDelete = async () => {
+    if (!product) return;
+
+    await deleteDocument(product.id);
+
+    dispatch(toggleRemoveProduct(null));
+  };
+
   return (
     <Modal
       isOpen={isVisible}
       onClose={() => dispatch(toggleRemoveProduct(null))}
       title="Remover Produto"
     >
-      <h1>Teste</h1>
+      <p className="h2">{`Você tem certeza que deseja remover ${product?.name}`}</p>
+      <div className={styles.control}>
+        <button onClick={() => dispatch(toggleRemoveProduct(null))}>
+          Cancelar
+        </button>
+        <button onClick={handleDelete}>Remover</button>
+      </div>
     </Modal>
   );
 };
