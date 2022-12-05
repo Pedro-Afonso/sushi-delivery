18cb85a89851e5f17b9ea0f52758d136a258531e
Pedro-Afonso <pedro.766@hotmail.com>
2022-12-05 09:12:06 -0300

    feat: Add product information

--- src/components/AllProducts/ProductCard/ProductCard.tsx
+++ src/components/AllProducts/ProductCard/ProductCard.tsx
@@ -5,6 +5,8 @@ import { IProduct } from "../../interface";
 import { useMenu } from "../../../hooks";
 
 import styles from "./ProductCard.module.scss";
+import { getCurrency } from "../../../utils";
+import Image from "next/image";
 
 interface IProductCardProps {
   product: IProduct;
@@ -13,15 +15,36 @@ interface IProductCardProps {
 export const ProductCard: React.FC<IProductCardProps> = ({ product }) => {
   const { isVisible, toggleMenuHandler } = useMenu();
 
+  const price = getCurrency(product.price);
+
   return (
-    <>
-      <div key={product.id} className={styles.card}>
-        <div className={styles.edit}>
-          <HiDotsVertical className="icon hover" onClick={toggleMenuHandler} />
-          {isVisible && <ProductMenu toggleMenuHandler={toggleMenuHandler} />}
-        </div>
-        <h2>{product.name}</h2>
+    <div key={product.id} className={styles.card}>
+      <div className={styles.edit}>
+        <HiDotsVertical className="icon hover" onClick={toggleMenuHandler} />
+        {isVisible && (
+          <ProductMenu data={product} toggleMenuHandler={toggleMenuHandler} />
+        )}
       </div>
-    </>
+      <Image
+        priority
+        src={product.url}
+        alt={product.name}
+        width={100}
+        height={100}
+      />
+      <h2>{product.name}</h2>
+      <p>
+        <label>Preço: </label>
+        {price}
+      </p>
+      <p>
+        <label>Descrição: </label>
+        {product.description}
+      </p>
+      <p>
+        <label>Ingredientes: </label>
+        {product.ingredients.join(", ")}
+      </p>
+    </div>
   );
 };
